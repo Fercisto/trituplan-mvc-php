d3a959bfe03e05e4988cbcf02f13595a9f1fee78
Fercisto <dedtroll314@hotmail.com>
2026-01-31 11:06:29 -0600

    Manteniendo consistencia en label de botones de acción

--- views/admin/index.php
+++ views/admin/index.php
@@ -132,7 +132,7 @@
                                         </a>
 
                                         <a href="admin/cotizacion/actualizar?id=<?php echo s($cotizacion->id); ?>" class="admin__tabla--enlace admin__tabla--enlace-actualizar">
-                                            <i class="fa-solid fa-pen-to-square"></i> Editar
+                                            <i class="fa-solid fa-pen-to-square"></i> Actualizar
                                         </a>
 
                                         <form method="POST" class="w100" action="/admin/cotizacion/eliminar">
